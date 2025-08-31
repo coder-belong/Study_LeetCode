5e1ed3619fb742135ba8110a4fc2e463d3fc693a
belong <33158742@qq.com>
2025-08-31 21:05:43 +0800

    feat: 搭建项目框架

---
+++ src/01-array/01_两数之和.ts
@@ -0,0 +1,13 @@
+console.log(twoSum([2, 7, 11, 15], 9)); // 输出 [0,1]
+
+export function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement)!, i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
